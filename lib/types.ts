67f26d8ad59782843deaca76f0f88eee6550b586
tom1k <103222032+tom1k77@users.noreply.github.com>
2025-11-19 15:45:30 +0300

    Create types.ts

---
+++ lib/types.ts
@@ -0,0 +1,20 @@
+export interface Token {
+  token_address: string;
+  name?: string;
+  symbol?: string;
+  source?: string;
+  source_url?: string;
+  first_seen_at?: string | null;
+
+  farcaster_url?: string | null;
+  website_url?: string | null;
+  x_url?: string | null;
+  telegram_url?: string | null;
+}
+
+export interface TokenWithMarket extends Token {
+  price_usd?: number | null;
+  market_cap_usd?: number | null;
+  liquidity_usd?: number | null;
+  volume_24h_usd?: number | null;
+}
