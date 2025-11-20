2914f8eab7efbd76f6306515b98f86d850fe46a7
tom1k <103222032+tom1k77@users.noreply.github.com>
2025-11-20 13:00:50 +0300

    Create db.ts

---
+++ lib/db.ts
@@ -0,0 +1,11 @@
+// lib/db.ts
+import { Pool } from "pg";
+
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL is not set");
+}
+
+export const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  // Можно добавить ssl: { rejectUnauthorized: false } для Supabase/Neon, если нужно
+});
